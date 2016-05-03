fe34cb13ae0c4f194f0473b74cedd01b201bb773
Diego Perez <alphamander@gmail.com>
2016-05-02 19:05:57 -0600

    Start menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,47 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.IO;
-public class GameOver : MonoBehaviour {
 
-	public Texture gameOverTexture;
+public class MainMenu : MonoBehaviour {
+
+
+	public Texture mainMenuTexture;
 	Texture2D texture = null;
+	public TextAsset imageAsset;
 
 	// Use this for initialization
 	void Start () {
 		#if UNITY_WEBPLAYER
+		texture = new Texture2D(1024, 768);
+		texture.LoadImage(imageAsset.bytes); 
 		print("Not Going To Read That File!");
 		#else
-		byte[] fileData = File.ReadAllBytes ("Assets/Images/game_over.jpg");
+		byte[] fileData = File.ReadAllBytes ("Assets/Images/main_menu.jpg");
 		texture = new Texture2D(1024, 768);
 		texture.LoadImage(fileData); 
 		#endif
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	void OnGUI()
 	{
 		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), texture);
 
-		// Try again
+		// New Game
 		if (GUI.Button(
-				new Rect(Screen.width / 2, Screen.height / 2 + 40, 150, 25),
-				"Try again"
-			)
+			new Rect(Screen.width / 2, Screen.height / 2 + 40, 150, 25),
+			"New Game"
+		)
 		) {
+			Debug.Log("hola");
+			Application.LoadLevel("Level 1");
+		}
+
+		// Stage Selection
+		if (GUI.Button(
+			new Rect(Screen.width / 2, Screen.height /2 + 70, 150, 25),
+			"Stage Selection"
+		)
+		)  {
 			Application.LoadLevel("Level 1");
 		}
 
 		// Quit
 		if (GUI.Button(
-			new Rect(Screen.width / 2, Screen.height /2 + 70, 150, 25),
+			new Rect(Screen.width / 2, Screen.height /2 + 100, 150, 25),
 			"Quit"
-			)
+		)
 		)  {
-			Application.LoadLevel("MainMenu");
+			Application.Quit();
 		}
 	}
 }
