6627721a344168a2487b8a286deacbe28f69abc0
Ceborz <richie.zepeda@gmail.com>
2016-05-09 19:27:59 -0600

    Pausa, bien hecha

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -5,9 +5,76 @@ public class GunScript : MonoBehaviour {
 	public GameObject bullet;
 	public float bulletForce = 90f;
 	public float bulletTime = 10f;
+	private bool firstPause;
+	private GameObject Temporary_Bullet_Handler;
+	private Vector3[] eVelocities;
+	private Vector3[] pVelocities;
+	private float tempForce; 
+	private GameObject[] eBullets;
+	private GameObject[] pBullets;
+
+	void Start(){
+		firstPause = false;
+	}
+
+	void Update(){
+		if (Globals.paused && !firstPause) {
+			firstPause = true;
+			OnPauseGame ();
+		}
+		if ((!Globals.paused) && firstPause) {
+			firstPause = false;
+			OnResumeGame ();
+		}
+	}
+
+	void OnPauseGame(){
+		if (Temporary_Bullet_Handler != null) {
+			eBullets =  GameObject.FindGameObjectsWithTag("eBullet");
+			eVelocities = new Vector3[eBullets.Length];
+			StopBullet (eBullets, "enemy");
+			pBullets =  GameObject.FindGameObjectsWithTag("pBullet");
+			pVelocities = new Vector3[pBullets.Length];
+			StopBullet (pBullets, "player");
+		}
+	}
+
+	void OnResumeGame(){
+		GameObject[] eBullets =  GameObject.FindGameObjectsWithTag("eBullet");
+		WakeBullet (eBullets, "enemy");
+		GameObject[] pBullets =  GameObject.FindGameObjectsWithTag("pBullet");
+		WakeBullet (pBullets, "player");
+	}
+
+	void StopBullet(GameObject[] bullets, string type){
+		for (int i = 0; i < bullets.Length; i++) {
+			GameObject bullet = bullets [i];
+			Vector3 tempVelocity;
+			tempVelocity = bullet.GetComponent<Rigidbody> ().velocity;
+			if (type == "enemy") {
+				eVelocities [i] = tempVelocity;
+			} else if (type == "player") {
+				pVelocities [i] = tempVelocity;
+			}
+			bullet.GetComponent<Rigidbody> ().isKinematic = true;
+		}
+	}
+
+	void WakeBullet(GameObject[] bullets, string type){
+		for (int i = 0; i < bullets.Length; i++) {
+			GameObject bullet = bullets [i];
+			bullet.GetComponent<Rigidbody> ().isKinematic = false;
+			Vector3 tempVelocity = new Vector3();
+			if (type == "enemy") {
+				tempVelocity = eVelocities [i];
+			} else if (type == "player") {
+				tempVelocity = pVelocities [i];
+			}
+			bullet.GetComponent<Rigidbody>().AddForce( tempVelocity, ForceMode.VelocityChange );
+		}
+	}
 
 	void Fire(GameObject gameobject){
-		GameObject Temporary_Bullet_Handler;
 
 		Vector3 emitter = new Vector3 (
 			gameobject.transform.position.x,
